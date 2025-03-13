c57daa4cd0c46d595481b086d0c51050d3720390
Craig Goodman <Craig.7144@gmail.com>
2025-03-13 14:56:45 -0600

    yo

--- www/env.js
+++ www/env.js
@@ -1,3 +1,3 @@
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-4vqasb471xfpojzq'
+export const clientId = '0aqnZcTFfRKPhNQNkRG3HTMoulAoqTdm'
+export const audience = 'https://auth.speedwagonfoundation.com'
